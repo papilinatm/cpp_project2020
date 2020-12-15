fce0b5450b6c7e388784fb224bcfb1456120ad38
PsiWa <serezfed@gmail.com>
2020-12-15 16:01:07 +0300

    Generators for timsort

--- sort_race/DataGenerators.h
+++ sort_race/DataGenerators.h
@@ -3,3 +3,6 @@ std::vector<int> MyGenerate(int size);
 std::vector <int> GenerateBestDataRadixLSD(int size);
 std::vector <int> GenerateWorstDataRadixLSD(int size);
 std::vector <int> GenerateNormalDataRadixLSD(int size);
+std::vector<int> GenerateNormalDataTimsort(int size);
+std::vector<int> GenerateBestDataTimsort(int size);
+std::vector<int> GenerateWorstDataTimsort(int size);
--- sort_race/DataGeneratorts.cpp
+++ sort_race/DataGeneratorts.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <algorithm>
 #include "DataGenerators.h"
 
 using namespace std;
@@ -15,12 +16,49 @@ vector <int> GenerateBestDataRadixLSD(int size) {
 	for (int& i : data)
 		i = rand() % 10;
 	return data;
+
+// GENERATORS OF DIFFERENT INPUT DATA SCENARIOS FOR TIMSORT
+vector<int> GenerateNormalDataTimsort(int size)
+{
+    vector<int> data;
+    while (data.size() < size)
+    {
+        int sequance = rand() % 512;
+        vector<int> seq(sequance);
+        for (int& d : seq)
+            d = rand() % INT_MAX;
+        sort(seq.begin(), seq.end());
+        data.insert(data.end(), seq.begin(), seq.end());
+    }
+    data.resize(size);
+    return data;
 }
 vector <int> GenerateWorstDataRadixLSD(int size) {
 	vector <int>data(size);
 	for (int& i : data)
 		i = 1000000+rand()%8000000;
 	return data;
+
+vector<int> GenerateBestDataTimsort(int size)
+{
+    int i = rand() % INT_MAX/4;
+    vector<int> data(size);
+    for (int& d : data)
+        d = i++;
+    return data;
+}
+
+vector<int> GenerateWorstDataTimsort(int size)
+{
+    int i = 2 + rand() / INT_MAX;
+    vector<int> data(size);
+    bool Flag = true;
+    for (int& d : data)
+    {
+        d = (Flag) ? i-- : i++;
+        Flag = !Flag;
+    }
+    return data;
 }
 vector <int> GenerateNormalDataRadixLSD(int size) {
 	vector <int>data(size);
