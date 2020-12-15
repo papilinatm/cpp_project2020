49ee90898b83fc002161fafc4a232dc1e2bed7fb
concrete-m1xer <61244161+concrete-m1xer@users.noreply.github.com>
2020-12-15 14:58:23 +0300

    Проверки
    Добавил перегрузки для double и проверку для отрицательных чисел в counting, ведь это недопустимо

--- sort_race/CountingSortByTkachev.cpp
+++ sort_race/CountingSortByTkachev.cpp
@@ -2,16 +2,20 @@
 
 using namespace std;
 
-vector<int> CountingSortByTkachev(vector<int> array)
+vector<int> CountingSortByTkachev(vector<int> data)
 {
-	int max = array[0];
-	for (size_t i = 1; i < array.size(); i++)
+	int max = data[0];
+	for (size_t i = 0; i < data.size(); i++)
 	{
-		max = (array[i] > max) ? array[i] : max;
+		max = (data[i] > max) ? data[i] : max;
+		if (data[i] < 0)
+		{
+			return data; // не может работать с массивами, где есть хоть одно отрицательное число
+		}
 	}
 	vector<int> c;
 	c.resize(max + 1);
-	for (int elem : array)
+	for (int elem : data)
 	{
 		++c[elem];
 	}
@@ -20,9 +24,14 @@ vector<int> CountingSortByTkachev(vector<int> array)
 	{
 		for (size_t j = 0; j < c[i]; j++)
 		{
-			array[b] = i;
+			data[b] = i;
 			b++;
 		}
 	}
-	return array;
+	return data;
+}
+
+vector<double> CountingSortByTkachev(vector<double> data)
+{
+	return data; // не будет работать с double, так как принципиально построен на располжении кол-ва чисел в соответствующий элемент массива
 }
--- sort_race/TimSortByTkachev.cpp
+++ sort_race/TimSortByTkachev.cpp
@@ -100,3 +100,102 @@ vector<int> TimSortByTkachev(vector<int> data)
 	}
 	return data;
 }
+
+vector<double> TimSortByTkachev(vector<double> data)
+{
+	//----------------------------------------
+	int minrun = 0;
+	if (data.size() < 64)
+	{
+		minrun = data.size();
+	}
+	else
+	{
+		int n = data.size();
+		int r = 0;
+		while (n > 64)
+		{
+			n >>= 1; // побитовый сдвиг n на один
+		}
+		r |= n & 1;// в r записывается результат побитного ИЛИ между прошлым значением r 
+			//и побитного И для оставшихся битов n и 1 (если в битах есть 1, то правда)
+		minrun = n + r;
+	}
+	//----------------------------------------
+	int num = data.size() / minrun;
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = (i * minrun) + 1; j < ((i + 1) * minrun); j++)
+		{
+			int temp = data[j];
+			int k = j - 1;
+			while (k >= (i * minrun) && data[k] > temp)
+			{
+				data[k + 1] = data[k];
+				--k;
+			}
+			data[k + 1] = temp;
+		}
+	}
+	for (int i = data.size() - (data.size() % minrun); i < data.size(); i++)
+	{
+		int temp = data[i];
+		int j = i - 1;
+		while (j >= data.size() % minrun && data[j] > temp)
+		{
+			data[j + 1] = data[j];
+			--j;
+		}
+		data[j + 1] = temp;
+	}
+	//-----------------------------------------------------------
+	int LeftRunIt;
+	int RightRunIt;
+	int MergeIterator;
+	int LeftBorder;
+	int MidBorder;
+	int RightBorder;
+	vector<int> temporary;
+	for (int BlockSize = minrun; BlockSize < data.size(); BlockSize *= 2)
+	{
+		for (int StartRun = 0; StartRun < data.size() - BlockSize; StartRun += 2 * BlockSize)
+		{
+			temporary.clear();
+			LeftRunIt = 0;
+			RightRunIt = 0;
+			LeftBorder = StartRun;
+			MidBorder = StartRun + BlockSize;
+			RightBorder = StartRun + 2 * BlockSize;
+			if (RightBorder >= data.size())
+			{
+				RightBorder = data.size();
+			}
+			temporary.reserve(RightBorder - LeftBorder);
+			while (LeftBorder + LeftRunIt < MidBorder && MidBorder + RightRunIt < RightBorder)
+			{
+				if (data[LeftBorder + LeftRunIt] < data[MidBorder + RightRunIt])
+				{
+					temporary.push_back(data[LeftBorder + LeftRunIt++]);
+				}
+				else
+				{
+					temporary.push_back(data[MidBorder + RightRunIt++]);
+				}
+			}
+			while (LeftBorder + LeftRunIt < MidBorder)
+			{
+				temporary.push_back(data[LeftBorder + LeftRunIt++]);
+			}
+			while (MidBorder + RightRunIt < RightBorder)
+			{
+				temporary.push_back(data[MidBorder + RightRunIt++]);
+			}
+
+			for (int MergeIterator = 0; MergeIterator < LeftRunIt + RightRunIt; MergeIterator++)
+			{
+				data[LeftBorder + MergeIterator] = temporary[MergeIterator];
+			}
+		}
+	}
+	return data;
+}
