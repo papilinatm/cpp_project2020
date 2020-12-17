ed10b3757f3de391b74a756eafd3cc0db980dcba
Kirill0077 <67799834+Kirill0077@users.noreply.github.com>
2020-12-17 17:33:02 +0300

    добавление наборов данных для сортировки BitonicSort

--- sort_race/DataSetRegister.h
+++ sort_race/DataSetRegister.h
@@ -14,6 +14,9 @@ std::vector<DataSet<int>> GenerateIntDataSets()
         { "random integer", GenerateData(10) },
         { "random integer", GenerateData(100) },
         { "random integer", GenerateData(1000) },
+        { "random integer for BitonicSortBySharnin", GenerateData(8) }, //to sort Bitonik it is necessary that the array contains 2 ^ n elements 
+        { "random integer for BitonicSortBySharnin", GenerateData(128) },
+        { "random integer for BitonicSortBySharnin", GenerateData(1024) },
     };
 }
 std::vector<DataSet<double>> GenerateDoubleDataSets()
