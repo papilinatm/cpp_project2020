e31bb507633b0adba4251bee7ff2822198a127ca
bogdancpp <kononenko.aa1905@asugubkin.ru>
2020-12-20 19:02:30 +0400

    add 3 set generate data
    1. Negative
    2.Semi-sorted
    3.Full sorted

--- sort_race/DataSets.h
+++ sort_race/DataSets.h
@@ -9,3 +9,12 @@ std::vector<double> GenerateDoubleData();
 //datasets by <comandname>
 //place your datasets here 
 //or include your "datasets_by_<comandname>.h" above
+
+// Kononenko team 
+std::vector<int> NegativeIntegerGenerateData(int size, int max_value = INT_MAX);
+std::vector<double> NegativeDuobleGenerateData(double size, int max_value = INT_MAX);
+std::vector<int> SemiSortedIntegerGenerateData(int size, int max_value = INT_MAX);
+std::vector<double> SemiSortedDoubleGenerateData(double size, int max_value = INT_MAX);
+std::vector<int> FullSortedIntegerGenerateData(int size, int max_value = INT_MAX);
+std::vector<double> FullSortedDoubleGenerateData(double size, int max_value = INT_MAX);
+
