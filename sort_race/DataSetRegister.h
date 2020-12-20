81946bb8e8aef9e120d899a889199ccefd5e2a75
IrinaShekhovtsova <irinahaess@mail.ru>
2020-12-20 19:39:39 +0300

    sort_race READY

--- sort_race/DataSetRegister.h
+++ sort_race/DataSetRegister.h
@@ -14,6 +14,13 @@ std::vector<DataSet<int>> GenerateIntDataSets()
         { "random integer", GenerateData(10) },
         { "random integer", GenerateData(100) },
         { "random integer", GenerateData(1000) },
+        { "partially sorted integer", Data1_by_Shekhovtsova_team() },
+        { "negative and positive integer", Data2_by_Shekhovtova_team(100) },
+        { "negative and positive integer", Data2_by_Shekhovtova_team(1000) },
+        { "repetitive integer", Data3_by_Shekhovtsova_team(1000) },
+        { "repetitive integer", Data3_by_Shekhovtsova_team(10000) },
+        { "decreasing integer", Data4_by_Shekhovtsova_team(1000) },
+        { "decreasing integer", Data4_by_Shekhovtsova_team(100) },
     };
 }
 std::vector<DataSet<double>> GenerateDoubleDataSets()
